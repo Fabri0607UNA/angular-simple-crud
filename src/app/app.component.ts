94d31df22b352436c795729e37d7e34af2b36bb2
FabriUNA0607 <fabricio.alfaro.arce@est.una.ac.cr>
2024-05-31 14:40:43 -0600

    CRUD Completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule], // Agrega FormsModule aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    {id: 1, name: "John", country: "USA"},
+    {id: 2, name: "Smith", country: "UK"},
+    {id: 3, name: "Jack", country: "AUS"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
