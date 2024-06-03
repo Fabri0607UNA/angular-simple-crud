ce6508af21f3ae8f42e572117ba8e578e8a4611b
FabriUNA0607 <fabricio.alfaro.arce@est.una.ac.cr>
2024-06-03 13:17:25 -0600

    Versión FInal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
+import { Component, OnInit } from '@angular/core';
 import { Employee } from './models/employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule], // Agrega FormsModule aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  employeeArray: Employee[] = [
-    {id: 1, name: "John", country: "USA"},
-    {id: 2, name: "Smith", country: "UK"},
-    {id: 3, name: "Jack", country: "AUS"}
-  ];
-
+export class AppComponent implements OnInit {
+  employeeArray: Employee[] = [];
   selectedEmployee: Employee = new Employee();
 
-  addOrEdit(){
-    if(this.selectedEmployee.id === 0){
-      this.selectedEmployee.id = this.employeeArray.length + 1;
-      this.employeeArray.push(this.selectedEmployee);
-    }
-    this.selectedEmployee = new Employee();
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
   }
 
-  openForEdit(employee: Employee){
+  getEmployees() {
+    this.employeeService.getEmployees().subscribe((employees) => {
+      this.employeeArray = employees;
+    });
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.employeeService.addEmployee(this.selectedEmployee).subscribe(() => {
+        this.getEmployees();
+        this.selectedEmployee = new Employee();
+      });
+    } else {
+      this.employeeService.updateEmployee(this.selectedEmployee).subscribe(() => {
+        this.getEmployees();
+        this.selectedEmployee = new Employee();
+      });
+    }
+  }
+
+  openForEdit(employee: Employee) {
     this.selectedEmployee = employee;
   }
 
-  delete(){
-    if(confirm('Are you sure you want to delete it?')){
-      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
-      this.selectedEmployee = new Employee();
+  delete() {
+    if (confirm('Are you sure you want to delete it?')) {
+      this.employeeService.deleteEmployee(this.selectedEmployee.id).subscribe(() => {
+        this.getEmployees();
+        this.selectedEmployee = new Employee();
+      });
     }
   }
-
 }
